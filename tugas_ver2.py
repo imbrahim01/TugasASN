175407bc935fb1afde851dc405460bed9ca2e9e2
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-20 19:09:53 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -309,8 +309,8 @@ if selected == "PAGE 4":
         st.write(pNN50)
     elif selected_optimizer1 == 'SDSD':
         st.write(SDSD)
-  fig_Tachogram = go.Figure(data=go.Scatter(x=n, y=bpm_rr, mode='lines'))
-  fig_Tachogram.update_layout(
+fig_Tachogram = go.Figure(data=go.Scatter(x=n, y=bpm_rr, mode='lines'))
+fig_Tachogram.update_layout(
       title="TACHOGRAM",
       xaxis_title="n",
       yaxis_title="BPM",
@@ -319,9 +319,9 @@ if selected == "PAGE 4":
   )
   st.plotly_chart(fig_Tachogram)
 
-  fig_histogram = go.Figure(data=go.Histogram(x=bpm_rr, nbinsx=ptp))
+fig_histogram = go.Figure(data=go.Histogram(x=bpm_rr, nbinsx=ptp))
 
-  fig_histogram.update_layout(
+fig_histogram.update_layout(
       title="Histogram Interval RR",
       xaxis_title="Interval RR",
       yaxis_title="Banyak Data",
@@ -331,7 +331,7 @@ if selected == "PAGE 4":
       bargroupgap=0.1, # Optional: Adjusts the gap between groups
   )
 
-  st.plotly_chart(fig_histogram)
+st.plotly_chart(fig_histogram)
 
     
 
