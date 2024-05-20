4b0c1e31d84fd88ffbab3f25973ea68e42e0be5d
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-20 15:05:16 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -22,7 +22,7 @@ with st.sidebar:
 if selected == "Home":
    st.title('Project ASN Kelompok 1')
    st.subheader("Anggota kelompok")
-   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">"Farhan Majid Ibrahim - 5023211049"</p>'
+   new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">"Farhan Majid Ibrahim - 5023211049</p>'
    st.markdown(new_title, unsafe_allow_html=True)
    new_title = '<p style="font-family:Georgia; color: black; font-size: 15px;">Nayla Pramudhita Putri Pertama - 5023211012</p>'
    st.markdown(new_title, unsafe_allow_html=True)
