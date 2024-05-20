0c252bd6f541fb865f53f19b35a61a3412051c79
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-20 22:49:32 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -186,7 +186,7 @@ if selected == "Ecyclopedia":
     st.markdown("<h1 style='text-align: center; color: red;'>🫀ECYCLOPEDIA </h1>", unsafe_allow_html=True)
     new_title = '<p style="font-family:Georgia; color:blue; font-size: 23px; text-align: center;">Apa yang dimaksud HRV?</p>'
     st.markdown(new_title, unsafe_allow_html=True)
-    st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json", text-align: center, height=400, width=400)
+    st_lottie("https://assets5.lottiefiles.com/packages/lf20_V9t630.json", text-align= center, height=400, width=400)
 
 
     
