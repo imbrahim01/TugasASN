c433b029eff95254c714d4ac643c8c6f22417cab
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-20 14:55:59 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -26,6 +26,7 @@ if selected == "Home":
    st.text("Nayla Pramudhita Putri Pertama - 5023211012")
    st.text("Mohammad Rayhan Amirul Haq Siregar - 5023211045")
    st.text("Reynard Prastya Savero - 5023211042")
+   new_title = '<p style="font-family:Georgia; color:#FF0000; font-size: 23px;">Reynard Prastya Savero - 5023211042</p>'
 
 if selected == "PAGE 1":
     st.title('Data & Graphic Input')
