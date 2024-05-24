3a89499afe4bd5b24f8b090f0ce464779a61f16c
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-25 00:21:05 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -169,7 +169,7 @@ n = np. arange(0,ptp,1,dtype=int)
 
 
 with st.sidebar:
-    selected = option_menu("TUGAS 1", ["Home","Ecyclopedia", "Data & Graphic", "Filter","Method","HRV Analysis"], icons=['house', 'earth'], menu_icon="cast", default_index=1)
+    selected = option_menu("TUGAS 1", ["Home","Ecyclopedia", "Data & Graphic", "Filter","Method","HRV Analysis"], icons=['house', 'book'], menu_icon="cast", default_index=1)
 
 if selected == "Home":
    st.title('Project ASN Kelompok 1')
