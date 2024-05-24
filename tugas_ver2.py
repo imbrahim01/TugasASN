942885dd9bd54d99fd11456bac0077dec762a2d4
SAVERO999 <167954392+SAVERO999@users.noreply.github.com>
2024-05-24 22:45:17 +0700

    Update tugas_ver2.py

--- tugas_ver2.py
+++ tugas_ver2.py
@@ -204,6 +204,7 @@ if selected == "Ecyclopedia":
      <iframe id='Video 1' width='560' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>
      
      """
+    st.markdown(content, unsafe_allow_html=True)
 
 
 
